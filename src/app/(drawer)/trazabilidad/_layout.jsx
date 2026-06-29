27db21f3352b2c6cb1a87ee5787b6fad48123e44
Ariana Araya <arianita593@gmail.com>
2026-06-28 23:09:12 -0400

    Update _layout.jsx
    cambio del <Stack screenOptions={{ headerShown: true }}>
    
    a
    
    <Stack screenOptions={{ headerShown: false }}>
    
    para evitar doble header y mejor consistencia en el diseño.

--- src/app/(drawer)/trazabilidad/_layout.jsx
+++ src/app/(drawer)/trazabilidad/_layout.jsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function TrazabilidadStackLayout() {
   return (
-    <Stack screenOptions={{ headerShown: true }}>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ title: "Sección de Trazabilidad" }} />
 
       <Stack.Screen name="agregar" options={{ title: "Registrar Trazabilidad" }} />
